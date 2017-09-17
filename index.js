f745caee465fe1c4a777a9822abdf70582343593
Olena Ageyeva <eageeva@hotmail.com>
2017-09-17 18:12:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function checkCollision(rock) {
                *    and the rock's right edge is > the DODGER's right edge
                */) {
       return true
-    } else {false}
+    } else {return false}
   }
 }
 
