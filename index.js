ccf0336f777485a08119fac37b4958fa0fb00a68
Olena Ageyeva <eageeva@hotmail.com>
2017-09-17 20:45:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ function createRock(x) {
                                 window.requestAnimationFrame(step)
                               }
                             }
-
+                            window.requestAnimationFrame(step)
 
 
     /**
