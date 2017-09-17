6ec31f398033e45f6626ceadad0805a4c7fe4582
Olena Ageyeva <eageeva@hotmail.com>
2017-09-17 20:35:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -129,7 +129,7 @@ function endGame() {
   clearInterval(gameInterval)
   window.removeEventListener("keydown", moveDodger)
   i1=0
-  var currentRock=document.getAllElemens(".rock")
+  var currentRock=document.getElemensByClassName("rock")
   for (var i=0; i<currentRock.length; i++){
     document.removeChild(currentRock[i])
   }
