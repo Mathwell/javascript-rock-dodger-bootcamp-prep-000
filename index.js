5701f3b9d05bf01c8a80165cce9edcf669c3a895
Olena Ageyeva <eageeva@hotmail.com>
2017-09-17 19:42:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ function createRock(x) {
 
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
-  window.requestAnimationFrame(step)
+  window.requestAnimationFrame()
   ROCKS.push(rock)
 
   // Finally, return the rock element you've created
