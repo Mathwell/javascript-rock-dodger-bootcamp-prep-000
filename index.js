436d0e2c34ab79df913affd74b90b6c07f5c3bf8
Olena Ageyeva <eageeva@hotmail.com>
2017-09-17 21:44:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -191,7 +191,7 @@ function moveDodgerRight() {
    var leftNumbers = dodger.style.left.replace('px', '')
    var left = parseInt(leftNumbers, 10)
 
-   if (left > 0) {
+   if (left < GAME_WIDTH-40) {
      dodger.style.left = `${left + 4}px`
    }
 }
